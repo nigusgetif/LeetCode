29c6c492527e7e77c53e8bbdb31e52b4475fdd62
Nigus Getie <94644207+nigusgetif@users.noreply.github.com>
2024-03-29 22:38:26 +0300

    Time: 1129 ms (17.46%), Space: 19.4 MB (72.52%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+          return None
+
+        def merge(left, right):
+            dummy = curr = ListNode(0)
+            while left and right:
+                if left.val < right.val:
+                    curr.next = left
+                    left = left.next
+                else:
+                    curr.next = right
+                    right = right.next
+                curr = curr.next
+
+            curr.next = left or right
+            return dummy.next
+
+        return reduce(merge, lists)
